d46432e7a0fdb9a88bafa47266ccdac0755f2086
its0su0rj <155565040+its0su0rj@users.noreply.github.com>
2024-01-16 02:12:45 +0530

    Update FLTI.py

--- FLTI.py
+++ FLTI.py
@@ -47,8 +47,8 @@ def diabetes_prediction(input_data):
 
 
 # Load the trained k-NN model and scaler
-model_knn = joblib.load('admission_probability_model.joblib')
-scaler = joblib.load('scaler.joblib')
+#model_knn = joblib.load('admission_probability_model.joblib')
+#scaler = joblib.load('scaler.joblib')
 
 # Load the trained model for image compression
 model_image_compression = joblib.load('image.joblib')
@@ -200,32 +200,12 @@ def ml_model_page():
         living_facilities = st.number_input("Enter Living Facilities (1 to 10):", min_value=1, max_value=10, step=1)
         girls_boys_ratio_percentage = st.number_input("Enter Girls/Boys Ratio Percentage (1 to 100):", min_value=1, max_value=100, step=1)
 
-        # Create a DataFrame with the input data
-        new_data = {
-            'All_India_Rank': [all_india_rank],
-            'NIRF_Ranking': [nirf_ranking],
-            'Placement_Percentage': [placement_percentage],
-            'Median_Placement_Package': [median_placement_package],
-            'Distance_from_College': [distance_from_college],
-            'Government_Funded': [1 if government_funded else 0],
-            'Teachers_Qualification': [teachers_qualification],
-            'College_Fee': [college_fee],
-            'Living_Facilities': [living_facilities],
-            'Girls_Boys_Ratio_Percentage': [girls_boys_ratio_percentage],
-        }
-
-        input_df = pd.DataFrame(new_data)
-
-        # Standardize the features using the same scaler used during training
-        new_data_scaled = scaler.transform(input_df)
-
+       
         # Button to trigger prediction
         if st.button("Percentage Probability to Join the College"):
             # Make predictions using the trained model
-            st.write(f"Input Features (scaled): {new_data_scaled}")
-            predicted_admission_probability = model.predict(new_data_scaled)
-            st.write(f'Predicted Admission Probability: {predicted_admission_probability[0]}%')
-
+            st.write("it will predict soon")
+            
 
     elif selected_tab == "Diabetes Prediction":
         st.write("You are on the Diabetes Prediction page.")
